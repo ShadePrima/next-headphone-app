547a8ef5dd27bd3cad89e62001e3ad4b5383000d
ShadePrima <aleksandrag1984@gmail.com>
2023-08-25 18:10:15 +0300

    header backgraund

---
+++ components/header/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import cl from 'clsx'
+
+import styles from './Header.module.scss'
+
+const Header = () => {
+  return <div className={cl(styles.main)}>Header</div>
+}
+
+export default Header
